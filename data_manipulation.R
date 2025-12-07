e410140516e22443dd90174251fd9c43f68bf79d
DanielDavid521 <15642147+Danieldavid521@users.noreply.github.com>
2025-12-07 12:04:23 -0800

    updated

--- data_manipulation.R
+++ data_manipulation.R
@@ -1,8 +1,12 @@
 #load libraries
+library(lubridate)
 
 #read in Dow jones data/file
+goldprice <- 4197
+dowjonesprice <- 47954
+date <- 2025-12-07
 #read in GOld data/file
-
+data <- data.frame(goldprice, dowjonesprice, date)
 #manipulate data into pretty format
-
+data$ratio <- (dowjonesprice/goldprice)
 #Save file
