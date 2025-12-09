32e85a59f37f51552d7939295f4e6119d2eda00f
DanielDavid521 <15642147+Danieldavid521@users.noreply.github.com>
2025-12-08 18:52:37 -0800

    Update data_manipulation.R
    adding web scraping

--- data_manipulation.R
+++ data_manipulation.R
@@ -1,7 +1,21 @@
 #load libraries
 library(lubridate)
+library(rvest)
+library(tidyverse)
 
 #read in Dow jones data/file
+dia_url <- 'https://finance.yahoo.com/quote/DIA/'
+gold_url <- read_html('https://finance.yahoo.com/quote/GC=F/')
+
+gold_scraped <- gold_url %>% html_elements(xpath = "//*[@id=\"main-content-wrapper\"]/section[1]/div[2]/div[1]/section/div/section/div[1]/div[1]/span")
+gold_url %>% html_elements(css="up1.yf-ipw1h0.base")
+head(gold_scraped)
+element:
+<span class="up1 yf-ipw1h0 base" data-testid="qsp-price">4,225.70</span>
+
+x path:
+  //*[@id="main-content-wrapper"]/section[1]/div[2]/div[1]/section/div/section/div[1]/div[1]/span
+<span class="up1 yf-ipw1h0 base" data-testid="qsp-price">4,225.70</span>
 goldprice <- 4197
 dowjonesprice <- 47954
 date <- 2025-12-07
