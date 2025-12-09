a4cfc8cbec0b35dcb30b47c5c63bf6b32f151b20
DanielDavid521 <15642147+Danieldavid521@users.noreply.github.com>
2025-12-08 19:04:23 -0800

    Update data_manipulation.R
    updated webscraping code

--- data_manipulation.R
+++ data_manipulation.R
@@ -4,12 +4,15 @@ library(rvest)
 library(tidyverse)
 
 #read in Dow jones data/file
-dia_url <- 'https://finance.yahoo.com/quote/DIA/'
+dia_url <- read_html('https://finance.yahoo.com/quote/DIA/')
 gold_url <- read_html('https://finance.yahoo.com/quote/GC=F/')
 
-gold_scraped <- gold_url %>% html_elements(xpath = "//*[@id=\"main-content-wrapper\"]/section[1]/div[2]/div[1]/section/div/section/div[1]/div[1]/span")
-gold_url %>% html_elements(css="up1.yf-ipw1h0.base")
-head(gold_scraped)
+gold_scraped <- gold_url %>% html_elements(xpath = "//*[@id=\"main-content-wrapper\"]/section[1]/div[2]/div[1]/section/div/section/div[1]/div[1]/span") %>%  html_text2()
+dia_scraped <- dia_url %>% html_elements(xpath =   "//*[@id=\"main-content-wrapper\"]/section[1]/div[2]/div[1]/section/div/section/div[1]/div[1]/span") %>%  html_text2()
+dia_scraped
+
+gold_url %>% html_elements(xpath="up1.yf-ipw1h0.base")
+gold_scraped
 element:
 <span class="up1 yf-ipw1h0 base" data-testid="qsp-price">4,225.70</span>
 
